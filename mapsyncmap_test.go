e7dce3059bc541ef3d1a63ba640764bda2f93816
ewan-carree <carree.e@outlook.com>
2022-07-08 11:09:46 +0200

    add new struct for specific shoset attributes

---
+++ mapsyncmap_test.go
@@ -0,0 +1,50 @@
+package shoset
+
+import (
+	"strconv"
+	"sync"
+	"testing"
+)
+
+var shoset *Shoset = NewShoset("cl", "cl")
+
+func TestStoreConfig(t *testing.T) {
+	shoset.InitPKI("127.0.0.1:9001")
+
+	direction := []string{IN, OUT}
+
+	for i := 2; i < 6; i++ {
+		conn, err := NewShosetConn(shoset, "127.0.0.1:900"+strconv.Itoa(i), direction[i%len(direction)])
+		if err != nil {
+			t.Errorf("StoreConfig didn't work, conn is nil")
+		}
+		err = shoset.ConnsByLname.StoreConfig("test", "127.0.0.1:900"+strconv.Itoa(i), "test_protocol", conn)
+		if err != nil {
+			t.Errorf("StoreConfig didn't work")
+		}
+		mapSync := new(sync.Map)
+		mapSync.Store("test", true)
+		shoset.LnamesByProtocol.Store("test_protocol", mapSync)
+	}
+}
+
+func TestKeys(t *testing.T) {
+	TestStoreConfig(t)
+
+	keys := shoset.ConnsByLname.Keys(ALL)
+	if keys[0] != "test" {
+		t.Errorf("Keys didn't work")
+	}
+}
+
+func TestDeleteConfig(t *testing.T) {
+	TestStoreConfig(t)
+
+	shoset.ConnsByLname.DeleteConfig("test", "127.0.0.1:9003")
+	shoset.ConnsByLname.DeleteConfig("test", "127.0.0.1:9004")
+
+	syncMap, _ := shoset.ConnsByLname.Load("test")
+	if LenSyncMap(syncMap.(*sync.Map)) != 2 {
+		t.Errorf("DeleteConfig didn't work")
+	}
+}
